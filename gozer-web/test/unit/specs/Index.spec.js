f2de5788c82afc7d47330f10eab6fb34138ded58
Jon Ross <jross@bypassmobile.com>
2017-02-15 11:18:33 -0600

    cleaning up the test files

---
+++ gozer-web/test/unit/specs/Index.spec.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Index from 'src/components/Index';
+
+describe('Index.vue', () => {
+  it('should render correct contents', () => {
+    /*TODO:figure out how to test these components*/
+    // const Constructor = Vue.extend(Hello);
+    // const vm = new Constructor().$mount();
+    // expect(vm.$el.querySelector('.hello h1').textContent)
+    //   .to.equal('Welcome to Your Vue.js App');
+  });
+});
