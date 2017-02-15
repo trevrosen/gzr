8a3702ce401b203c0342e9b5868a1e0f4fd78566
Jon Ross <jross@bypassmobile.com>
2017-02-15 13:16:42 -0600

    adding error handling for deployments

--- gozer-web/README.md
+++ gozer-web/README.md
@@ -28,3 +28,7 @@ npm test
 ```
 
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+Sad stay puft image from 
+https://www.etsy.com/people/Sugarfueledart
--- gozer-web/src/components/Index.vue
+++ gozer-web/src/components/Index.vue
@@ -1,22 +1,48 @@
 <template>
   <div>
-    <div class="gzr-cards">
-      <div class="flex-row">
-        <div v-for="deployment in list">
-          <deployment-dash-item :deployment="deployment"></deployment-dash-item>
+    <div v-if="loading">
+      <spinner v-model="loading" size="lg" fixed text="Loading Deployments"></spinner>
+    </div>
+    <div v-else-if="!error">
+      <div class="gzr-cards">
+        <div class="flex-row">
+          <div v-for="deployment in list">
+            <deployment-dash-item :deployment="deployment"></deployment-dash-item>
+          </div>
         </div>
       </div>
+    </div>
+    <div v-else>
+      <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+          <div class="media">
+            <div class="media-left media-middle">
+              <img src="../img/sad_stay_puft.png" class="media-object">
+            </div>
+            <div class="media-body media-middle">
+              <h3 class="media-heading">Gozer Error</h3>
+              Sorry there was an error loading deployments.
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
     </div>
   </div>
 </template>
 <script>
+  import {spinner} from 'vue-strap'
   import DeploymentService from '../services/DeploymentService'
   import DeploymentDashItem from './DeploymentDashItem'
   export default {
     name      : 'index',
     data ()  {
       return {
-        list: []
+        list   : [],
+        loading: true,
+        error  : null
       }
     },
     created() {
@@ -26,9 +52,16 @@
         .then(function (list) {
           vm.list = list;
         })
+        .catch(function (err) {
+          vm.error = err;
+        })
+        .finally(function () {
+          vm.loading = false;
+        })
     },
     components: {
-      DeploymentDashItem
+      DeploymentDashItem,
+      spinner
     }
   };
 </script>
