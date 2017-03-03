b714cd651b3c1341b06f61eb609f472c9667121d
Alex Seubert <alexseubert@gmail.com>
2017-03-03 10:24:27 -0600

    Fix MockStore functions

--- comms/mock_store.go
+++ comms/mock_store.go
@@ -2,17 +2,17 @@ package comms
 
 type MockStore struct {
 	OnStore   func(string, ImageMetadata) error
-	OnList    func(string) (ImageList, error)
+	OnList    func(string) (*ImageList, error)
 	OnCleanup func()
 	OnDelete  func(string) error
-	OnGet     func(string) (Image, error)
+	OnGet     func(string) (*Image, error)
 }
 
 func (mock *MockStore) Store(imageName string, meta ImageMetadata) error {
 	return mock.OnStore(imageName, meta)
 }
 
-func (mock *MockStore) List(imageName string) (ImageList, error) {
+func (mock *MockStore) List(imageName string) (*ImageList, error) {
 	return mock.OnList(imageName)
 }
 
@@ -24,6 +24,6 @@ func (mock *MockStore) Delete(imageName string) error {
 	return mock.OnDelete(imageName)
 }
 
-func (mock *MockStore) Get(imageName string) (Image, error) {
+func (mock *MockStore) Get(imageName string) (*Image, error) {
 	return mock.OnGet(imageName)
 }
